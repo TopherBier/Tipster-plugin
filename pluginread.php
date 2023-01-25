c108c19b9367a7a931866241ba9420fccc1f9134
トファー <50886709+TopherBier@users.noreply.github.com>
2023-01-25 13:40:19 +0300

    Create pluginread.php

---
+++ pluginread.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* Tipster-Module 5.0 - Developed By Christopher Obiero
+* Plugin URI: https://www.tophersolves.org/
+* Description: Test.
+* Version: 0.1
+* Author: your-name
+* Author URI: https://www.tophersolves.org/
+**/
